ce622355ef6ef1818ff32837b3c9f12a4af59cf9
Danilo Bürger <info@danilobuerger.de>
2017-03-17 20:21:59 +0100

    Added initial xcode project

---
+++ Coffeestats/Classes/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Coffeestats
+//
+//  Created by Danilo Bürger on 16/03/2017.
+//  Copyright © 2017 Danilo Bürger. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
